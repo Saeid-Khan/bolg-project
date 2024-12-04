89c96fd9bad65027218ca72a4e09f7a6a1f5da72
Saeid-Khan <150150740+Saeid-Khan@users.noreply.github.com>
2024-12-04 13:53:38 +0600

    blog

---
+++ index.js
@@ -0,0 +1,11 @@
+const menuIcon = document.getElementById("menu-icon");
+const menu = document.getElementById("menu"); 
+
+// @ts-ignore
+menuIcon.addEventListener("click", ()=> {
+    if(menu?.className === "hidden"){
+        menu.classList.remove("hidden");
+    }else{
+        menu?.classList.add("hidden")
+    }
+});
